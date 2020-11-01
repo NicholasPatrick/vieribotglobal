bf3b71a8b5164efcc08eab540763c9e6920bf1d6
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2020-11-01 12:10:34 +0700

    Making lagrange reveal fast.py faster
    Solves 50 digit numbers in under 0.5s 99% of the time.

--- Discord/lagrange reveal fast.py
+++ Discord/lagrange reveal fast.py
@@ -1,30 +1,45 @@
 import sys
 import json
-from math import sqrt
+from math import isqrt
 from random import randrange as random
 
 def lag(n):
     if(n==0):
         print("0 0 0 0")
-        return
-    mult=1;
-    while(n%4==0):
-        mult*=2
-        n//=4
-    i=int(sqrt(n))
-    while(True):
-        x=n-i*i
-        while(x>0 and x%4==0):x//=4
-        if(x>=0 and x%8!=7):
-            break
-        i-=1
+        return 1
+    r=0
+    while(n%2==0):
+        r+=1
+        n//=2
+    i=isqrt(n)
+    if(n%4==1):
+        if(i%2==1):
+            i-=1
+    else:
+        if(i%2==0):
+            i-=1
     n-=i*i
-    for j in range(n+1):
-        for k in range(j+1):
-            for l in range(k+1):
-                if(j*j+k*k+l*l==n):
-                    print(json.dumps(str(mult * l) + " " + str(mult * k)+" "+str(mult*j)+" "+str(mult*i)))
-                    return 1
+    sqrt2=isqrt(n)
+    for j in reversed(range(sqrt2+1)):
+        sqrt3=isqrt(n-j*j)
+        for k in reversed(range(sqrt3-100, sqrt3+1)):
+            l=isqrt(n-j*j-k*k)
+            if(k<l):
+                break
+            if(j*j+k*k+l*l==n):
+                if(r%2==1):
+                    if(i>j):
+                        i, j=j, i
+                    i, j=j-i, i+j
+                    if(k>l):
+                        k, l=l, k
+                    k, l=l-k, k+l
+                i<<=r//2
+                j<<=r//2
+                k<<=r//2
+                l<<=r//2
+                print(i, j, k, l)
+                return 1
     return 0
 
 
